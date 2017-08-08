2419149f559e5f735d14f7c4d52147b349e3e9b7
Darron Brown <DarronBrown88@outlook.com>
2017-08-08 01:23:15 -0600

    Building our First JSX React Component

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+
+const component = 
+	<div 
+		className="jumbotron container-fluid text-center" 
+		style={{ backgroundImage: "url(img/forest-1198698_1280.jpg)", color:"white"}}>
+    <div className="container">
+      <h1>Building React.js User Interfaces</h1>
+      <p>with Bootstrap and SASS.</p>
+      <p><a className="btn btn-primary" href="http://02geek.com/" target="_blank">Discover things.</a></p>
+    </div>
+  </div>;
+
+ReactDOM.render(
+	component,
+	document.getElementById('react')
+);
