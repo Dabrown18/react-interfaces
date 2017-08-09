7563761230fe492cf72103ad17d433e52d061983
Darron Brown <DarronBrown88@outlook.com>
2017-08-08 19:39:38 -0600

    Creating Reusable Utilities

--- src/bootstrap/Button.js
+++ src/bootstrap/Button.js
@@ -1,18 +1,14 @@
 import React, { Component } from 'react';
 
+import { classify } from './Utils'
+
 export default class Button extends Component {
-	classify(key, value) {
-		if(value[0] === '-') value = key + value;
-
-		value = value.split(' -').join(` ${key}-`);
-
-		return `${key} ${value}`;
-	}
+	
 
 	render() {
 
 		const Tag = this.props.href ? 'a' : 'button';
-		const className = this.classify('btn', this.props.className) +(this.props.disabled ? ' disabled' : '');
+		const className = classify('btn', this.props.className) +(this.props.disabled ? ' disabled' : '');
 
 		return (
 			<Tag {...this.props} className={className}>Discover things.</Tag>
