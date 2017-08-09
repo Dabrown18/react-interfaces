2473f75e913da3bd529f31d9300e3af4c0f45fdd
Darron Brown <DarronBrown88@outlook.com>
2017-08-08 18:56:27 -0600

    Understanding JSX Spread in Depth

--- src/App.js
+++ src/App.js
@@ -5,16 +5,15 @@ import Button from './bootstrap/Button';
 export default class App extends Component {
   render() {
     return (
-      <div 
-        className="jumbotron container-fluid text-center" 
-        style={{ backgroundImage: "url(img/forest-1198698_1280.jpg)", color:"white"}}>
+      <div className="jumbotron jumbotron-fulid text-xs-center" style={{backgroundImage:'url(img/forest-1198698_1280.jpg)',color:'white'}}>
         <div className="container">
           <h1>Building React.js User Interfaces</h1>
           <p>with Bootstrap and SASS.</p>
           <p>
-            <Button className="btn btn-primary" href="http://02geek.com/" target="_blank" />
-            <Button className="btn btn-danger" />
+            <Button className="btn-primary" href="http://02geek.com/" target="_blank" />
+            <Button className="btn-danger-outline btn-sm" />
           </p>
+          <Button className="btn-primary-outline btn-lg btn-block" />
         </div>
       </div>
     );
--- src/bootstrap/Button.js
+++ src/bootstrap/Button.js
@@ -4,9 +4,10 @@ export default class Button extends Component {
 	render() {
 
 		const Tag = this.props.href ? 'a' : 'button';
+		const className = 'btn ' + this.props.className;
 
 		return (
-			<Tag {...this.props}>Discover things.</Tag>
+			<Tag {...this.props} className={className}>Discover things.</Tag>
 		);
 	}
 }
