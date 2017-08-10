757129fff13c489678bfe5eed7951c2b0e6027a9
Darron Brown <DarronBrown88@outlook.com>
2017-08-09 18:28:54 -0600

    Building a Reusable Container Class

--- src/App.js
+++ src/App.js
@@ -2,20 +2,19 @@ import React, { Component } from 'react';
 
 import Button from './bootstrap/Button';
 import Jumbotron from './bootstrap/Jumbotron';
+import Container from './bootstrap/Container';
 
 export default class App extends Component {
   render() {
     return (
-      <Jumbotron className="-fulid text-xs-center" style={{backgroundImage:'url(img/forest-1198698_1280.jpg)',color:'white'}}>
-        <div className="container">
-          <h1>Building React.js User Interfaces</h1>
-          <p>with Bootstrap and SASS.</p>
-          <p>
-            <Button className="-primary" href="http://02geek.com/" target="_blank" disabled />
-            <Button className="-danger-outline -sm" disabled />
-          </p>
-          <Button className="-primary-outline -lg -block" disabled />
-        </div>
+      <Jumbotron className="-fluid text-xs-center" style={{backgroundImage:'url(img/forest-1198698_1280.jpg)',color:'white'}}>
+        <h1>Building React.js User Interfaces</h1>
+        <p>with Bootstrap and SASS.</p>
+        <p>
+          <Button className="-primary" href="http://02geek.com/" target="_blank" disabled />
+          <Button className="-danger-outline -sm" disabled />
+        </p>
+        <Button className="-primary-outline -lg -block" disabled />
       </Jumbotron>
     );
   }
--- src/bootstrap/Jumbotron.js
+++ src/bootstrap/Jumbotron.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import { classify } from './Utils';
+import Container from './Container';
 
 export default class Jumbotron extends Component {
 	render() {
 		return (
 			<div {...this.props} className={classify("jumbotron", this.props.className)}>
-				{this.props.children}
+				<Container>
+					{this.props.children}
+				</Container>
 			</div>
 		);
 	}
